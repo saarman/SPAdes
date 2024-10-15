c61198b9c71a1886e824e759228d2b3f6c4742e8
Norah Saarman <norah.saarman@usu.edu>
2024-10-15 13:38:47 -0600

    Update 4c_MMseqs2_search.pl

--- 4c_MMseqs2_search.pl
+++ 4c_MMseqs2_search.pl
@@ -19,31 +19,8 @@ my $rep_fasta = "$output_dir/clu_rep.fasta"; # Target
 my $out_m8 = "$output_dir/easyReps.m8"; # Search m8 output
 my $easy_search_cmd = "$mmseqs easy-search $ref_fasta $rep_fasta $out_m8 $tmp_dir --search-type 3"; # Search
 
-# Step 2: Generate a search db output from search with ref seqs against representatives for each cluster
-my $resultsDB = "$output_dir/searchRepsDB"; # Search results DB
-my $results_m8 = "$output_dir/searchReps.m8"; # Search results m8 out
-my $queryDB = "$output_dir/DB_refs"; # DB for query
-my $targetDB = "$output_dir/DB_rep"; # DB for subject, already exists!!!
-my $createdb1_cmd = "$mmseqs createdb $ref_fasta $queryDB"; # Create query DB
-my $createdb2_cmd = "$mmseqs createdb $rep_fasta $targetDB"; # Create target DB, already exists!!!
-my $createindex_cmd = "$mmseqs createindex $targetDB $tmp_dir"; # Index
-my $search_cmd = "$mmseqs search $queryDB $targetDB $resultsDB $tmp_dir --search-type 3"; # Search
-my $convertalis_cmd = "$mmseqs convertalis $queryDB $targetDB $resultsDB $results_m8"; # M8 output
-
-# Step 3: Execute the commands
+# Step 2: Execute the commands
 print "Running MMseqs2 easy-search...\n";
 system($easy_search_cmd) == 0 or die "MMseqs2 easy-search command failed: $?";
-
-print "Running MMseqs2 createdb1_cmd...\n";
-system($createdb1_cmd) == 0 or die "MMseqs2 createdb1_cmd command failed: $?";
-
-print "Running MMseqs2 createindex...\n";
-system($createindex_cmd) == 0 or die "MMseqs2 createindex command failed: $?";
-
-print "Running MMseqs2 search...\n";
-system($search_cmd) == 0 or die "MMseqs2 search command failed: $?";
-
-print "Running MMseqs2 convertalis...\n";
-system($convertalis_cmd) == 0 or die "MMseqs2 convertalis command failed: $?";
 
 print "MMseqs2 search tasks completed successfully.\n";
