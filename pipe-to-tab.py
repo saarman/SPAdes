babd86675731ff9bf291e36a019607f3b6628aaf
imaginee1 <46980139+imaginee1@users.noreply.github.com>
2025-07-07 13:48:03 -0600

    Update pipe-to-tab.py

--- pipe-to-tab.py
+++ pipe-to-tab.py
@@ -1,4 +1,4 @@
-with open("/uufs/chpc.utah.edu/common/home/saarman-group1/uphlfiles/SPAdes_scripts/blast_result/one-hit-test-bold-chordata.tsv", "r+", encoding="utf-8", errors="ignore") as f:
+with open("/uufs/chpc.utah.edu/common/home/saarman-group1/uphlfiles/SPAdes_scripts/blast_result/one_hit_final_bold-chordata-hits.m8", "r+", encoding="utf-8", errors="ignore") as f:
     data = f.read().replace("|", "\t")
     f.seek(0)
     f.write(data)
